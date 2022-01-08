3a7eacacef371270f2248079909575f0cb9dc9de
Angelina <angelinaivanova16@gmail.com>
2022-01-08 21:02:04 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let plus = Math.abs(n);
+  let str = plus.toString();
+  let arr = str.split( "" );
+  let rev = arr.reverse();
+  let result = rev.join( "" );
+  return result;
 }
